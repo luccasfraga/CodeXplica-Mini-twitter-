7dbb1c0b0910b01c9d949f5c9bd1eceef233f0f4
PatrickGmailHub <pgo.guedes@gmail.com>
2019-11-26 14:12:32 -0300

    Add Page/Step1

---
+++ src/pages/Step1/index.js
@@ -0,0 +1,13 @@
+import Header from '../../components/header';
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Step1 = () => (
+  <>
+    <Header background="blue" />
+    <h1>oie</h1>
+  </>
+);
+
+export default Home;
