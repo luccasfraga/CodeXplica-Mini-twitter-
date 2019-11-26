37ec136630c5a8aaf902658bf48be8d1d992de51
Luccas Fraga <luccas.fraga@ifood.com.br>
2019-11-26 14:19:21 -0300

    fix

--- src/pages/Step1/index.js
+++ src/pages/Step1/index.js
@@ -3,15 +3,11 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-<<<<<<< HEAD
 const Step1 = () => (
-=======
-const Home = () => (
->>>>>>> 82b57b8fdce73b439e95e86d96300278139218ca
   <>
     <Header background="blue" />
     <h1>oie</h1>
   </>
 );
 
-export default Home;
+export default Step1;
