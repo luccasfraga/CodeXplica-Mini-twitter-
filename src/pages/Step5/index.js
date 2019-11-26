b696be8f292cf80e1481b6420c3f75ec5249ff98
PatrickGmailHub <pgo.guedes@gmail.com>
2019-11-26 18:12:42 -0300

    Criando Steps 3 e 5 e respectivas rotas

---
+++ src/pages/Step5/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Container, Content, Result } from './styles';
+
+import Button from '../../components/button';
+import CardInfo from '../../components/cardInfo';
+import CodeEditor from '../../components/codeEditor';
+import Header from '../../components/header';
+import Subtitle from '../../components/subtitle';
+
+const Step5 = props => {
+  function handleClick() {
+    props.history.push('/step06');
+  }
+
+  return (
+    <>
+      <Header {...props} />
+      <Container>
+        <CardInfo
+          width="50%"
+          background="#f9f9f9"
+          text="Teste Component Teste Component Teste Component Teste Component Teste Component Teste Component Teste Component Teste Component Teste Component"
+        />
+
+        <Content>
+          <Subtitle
+            text="Copie o codigo ao lado para você ir para o próximo nível."
+            margin="0 0 30px"
+            align="left;"
+          />
+          <div>
+            <CodeEditor mode="html" />
+            <CodeEditor mode="html" readOnly value="<h1>oie</h1>" />
+          </div>
+
+          <Result>Resultado do code aqui....</Result>
+
+          <Button onClick={handleClick} text="Comparar" />
+        </Content>
+      </Container>
+    </>
+  );
+};
+
+export default Step5;
