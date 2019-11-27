2798f93bb27a2ac198009ef81d367fbb88fc979a
Filipe <flpgst@gmail.com>
2019-11-27 11:20:20 -0300

    ajustes css

--- src/components/cardInfo/styles.js
+++ src/components/cardInfo/styles.js
@@ -4,7 +4,7 @@ export const CardInfo = styled.p`
   display: flex;
   flex-direction: column;
   background-color: ${props => props.background};
-  height: 100vh;
+
   width: ${props => props.width};
   justify-content: center;
   padding: 40px;
@@ -19,6 +19,5 @@ export const CardInfo = styled.p`
     text-align: justify;
     text-justify: inter-character;
     line-height: 2;
-    height: 100vh;
-  }
+}
 `;
